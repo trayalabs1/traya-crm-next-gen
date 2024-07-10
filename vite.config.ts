27eb9b7cbca062efd3dc22e1329d4d8cfc0a36a8
Abdul <abdulmegoon@traya.health>
2024-07-10 10:58:02 +0530

    Alias setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return {
-    plugins: [react()],
+    plugins: [react(), tsconfigPaths()],
     define: {
       "process.env": env,
     },
