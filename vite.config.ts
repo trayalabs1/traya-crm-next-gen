e9e4e0b499e713bfcf705a56ac78c45527e37971
Abdul <abdulmegoon@traya.health>
2024-09-30 13:04:06 +0530

    Containerzed the app

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import tsconfigPaths from 'vite-tsconfig-paths';
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  
+
   return {
     plugins: [react(), tsconfigPaths()],
     define: {
-      'process.env': env,
+      "process.env": env,
     },
     server: {
+      host: "0.0.0.0",
+      strictPort: true,
       port: 3001,
     },
     preview: {
